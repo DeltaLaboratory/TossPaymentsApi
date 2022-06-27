fc64e758ab368e96b083e683c686cf722c26ce05
DeltaLaboratory <delta@deltalab.dev>
2022-06-27 23:32:28 +0900

    Updated Payment Model to v2022-06-08

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 [![Go Report Card](https://goreportcard.com/badge/github.com/DeltaLaboratory/TossPaymentsApi)](https://goreportcard.com/report/github.com/DeltaLaboratory/TossPaymentsApi)
 
 ## Description
-An API wrapper for the [Toss Payments API](https://docs.tosspayments.com/reference).
+ An API wrapper for the [Toss Payments API](https://docs.tosspayments.com/reference).
 
 ## Api Version
 | Release       | Api Version                                                                 | Supported          | Stable             |
 |---------------|-----------------------------------------------------------------------------|--------------------|--------------------|
 | v0.1.0        | [1.4](https://docs.tosspayments.com/reference/versioning#v14-)              | :white_check_mark: | :white_check_mark: |
  | [Main Branch] | [2022-06-08](https://docs.tosspayments.com/reference/versioning#2022-06-08) | :white_check_mark: | :x:                |
+ 
+## Change Log
+ v0.1.0: Initial release.
  
--- model.go
+++ model.go
@@ -5,33 +5,33 @@ package TossPaymentsApi
 // Definition : https://docs.tosspayments.com/reference#payment-%EA%B0%9D%EC%B2%B4
 // Version : 2022-06-08
 type Payment struct {
-	Version        string `json:"version"`        // Version of the payment object
-	PaymentKey     string `json:"paymentKey"`     // Unique key of transaction
-	OrderId        string `json:"orderId"`        // Unique ID issued by the merchant of order
-	OrderName      string `json:"orderName"`      // Name of the order
-	Type           string `json:"type"`           // Payment type information [ Possible Values : https://docs.tosspayments.com/reference/enum-codes#%EA%B2%B0%EC%A0%9C-%EC%88%98%EB%8B%A8 ]
-	Method         string `json:"method"`         // Payment method [ Possible Values : https://docs.tosspayments.com/reference/enum-codes#%EA%B2%B0%EC%A0%9C-%EC%88%98%EB%8B%A8 ]
-	MemberId       string `json:"mid"`            // Merchant ID
-	Currency       string `json:"currency"`       // Currency of the payment, MUST be "KRW"
-	TotalAmount    int64  `json:"totalAmount"`    // Total payment amount
-	BalanceAmount  int64  `json:"balanceAmount"`  // Cancelable Amount (current balance)
-	Status         string `json:"status"`         // Payment status [ Possible Values : "READY", "IN_PROGRESS", "WAITING_FOR_DEPOSIT", "DONE", "CANCELED", "PARTIAL_CANCELLED", "ABORTED", "EXPIRED" ]
-	RequestedAt    string `json:"requestedAt"`    // Requested time, [ISO 8601 format]
-	ApprovedAt     string `json:"approvedAt"`     // Approved time, [ISO 8601 format]
-	UseEscrow      bool   `json:"useEscrow"`      // Whether to use escrow service
-	TransactionKey string `json:"transactionKey"` // Unique key of transaction
-	SuppliedAmount int64  `json:"suppliedAmount"` // Supplied amount
-	Vat            int64  `json:"vat"`            // VAT amount
-	CultureExpense bool   `json:"cultureExpense"` // Whether to use culture expense
-	TaxFreeAmount  int64  `json:"taxFreeAmount"`  // Tax-free amount
+	Version        string  `json:"version"`        // Version of the payment object
+	PaymentKey     string  `json:"paymentKey"`     // Unique key of transaction
+	OrderId        string  `json:"orderId"`        // Unique ID issued by the merchant of order
+	OrderName      string  `json:"orderName"`      // Name of the order
+	Type           string  `json:"type"`           // Payment type information [ Possible Values : https://docs.tosspayments.com/reference/enum-codes#%EA%B2%B0%EC%A0%9C-%EC%88%98%EB%8B%A8 ]
+	Method         string  `json:"method"`         // Payment method [ Possible Values : https://docs.tosspayments.com/reference/enum-codes#%EA%B2%B0%EC%A0%9C-%EC%88%98%EB%8B%A8 ]
+	MemberId       string  `json:"mid"`            // Merchant ID
+	Currency       string  `json:"currency"`       // Currency of the payment, MUST be "KRW"
+	TotalAmount    float64 `json:"totalAmount"`    // Total payment amount
+	BalanceAmount  float64 `json:"balanceAmount"`  // Cancelable Amount (current balance)
+	Status         string  `json:"status"`         // Payment status [ Possible Values : "READY", "IN_PROGRESS", "WAITING_FOR_DEPOSIT", "DONE", "CANCELED", "PARTIAL_CANCELLED", "ABORTED", "EXPIRED" ]
+	RequestedAt    string  `json:"requestedAt"`    // Requested time, [ISO 8601 format]
+	ApprovedAt     string  `json:"approvedAt"`     // Approved time, [ISO 8601 format]
+	UseEscrow      bool    `json:"useEscrow"`      // Whether to use escrow service
+	TransactionKey string  `json:"transactionKey"` // Unique key of transaction
+	SuppliedAmount float64 `json:"suppliedAmount"` // Supplied amount
+	Vat            float64 `json:"vat"`            // VAT amount
+	CultureExpense bool    `json:"cultureExpense"` // Whether to use culture expense
+	TaxFreeAmount  float64 `json:"taxFreeAmount"`  // Tax-free amount
 	// Payment Cancellation History, NULLABLE
 	Cancels *[]struct {
-		CancelAmount     int64  `json:"cancelAmount"`     // Canceled amount
-		CancelReason     string `json:"cancelReason"`     // Canceled reason
-		TaxFreeAmount    int64  `json:"taxFreeAmount"`    // Tax-free amount
-		TaxAmount        *int64 `json:"taxAmount"`        // Tax amount, NULLABLE
-		RefundableAmount int64  `json:"refundableAmount"` // Refundable amount
-		CancelAt         string `json:"cancelAt"`         // Canceled time, [ISO 8601 format]
+		CancelAmount     float64  `json:"cancelAmount"`     // Canceled amount
+		CancelReason     string   `json:"cancelReason"`     // Canceled reason
+		TaxFreeAmount    float64  `json:"taxFreeAmount"`    // Tax-free amount
+		TaxAmount        *float64 `json:"taxAmount"`        // Tax amount, NULLABLE
+		RefundableAmount float64  `json:"refundableAmount"` // Refundable amount
+		CancelAt         string   `json:"cancelAt"`         // Canceled time, [ISO 8601 format]
 	} `json:"cancels"`
 	IsPartialCancelable bool `json:"isPartialCancelable"` // Whether partial cancellation is available
 	// Card information, NULLABLE
@@ -60,31 +60,39 @@ type Payment struct {
 	} `json:"virtualAccount"`
 	Secret      *string `json:"secret"` // Secret which is using for validate virtual account payment callback, NULLABLE
 	MobilePhone *struct {
-		Carrier             string `json:"carrier"`             // Deprecated: Deleted in 2020-06-08
+		Carrier             string `json:"carrier"`             // Deprecated: Deleted in Api Version 2020-06-08, SHOULD NOT BE USED
 		CustomerMobilePhone string `json:"customerMobilePhone"` // Mobile phone number which used for mobile purchase
 		SettlementStatus    string `json:"settlementStatus"`    // Settlement status of mobile phone payment, [ Possible Values : "INCOMPLETE", "COMPLETE" ]
 		ReceiptUrl          string `json:"receiptUrl"`          // Mobile phone payment receipt URL
-	}
+	} `json:"mobilePhone"` // Mobile phone payment information, NULLABLE
 	GiftCertificate *struct {
 		ApproveNo        string `json:"approveNo"`        // Approval number of gift certificate company
 		SettlementStatus string `json:"settlementStatus"` // Settlement status of gift certificate payment, [ Possible Values : "INCOMPLETE", "COMPLETE" ]
-	}
+	} `json:"giftCertificate"` // Gift certificate payment information, NULLABLE
 	Transfer *struct {
 		Bank             string `json:"bank"`             // Bank name of transfer payment
 		SettlementStatus string `json:"settlementStatus"` // Settlement status of transfer payment, [ Possible Values : "INCOMPLETE", "COMPLETE" ]
-	}
-	EasyPay     *string `json:"easyPay"` // EasyPay name information of payment, NULLABLE
-	Country     string  `json:"country"` // Country Information Paid
+	} `json:"transfer"` // Transfer payment information, NULLABLE
+	EasyPay *struct {
+		Amount         float64 `json:"amount"`         // Payment amount
+		Provider       string  `json:"provider"`       // EasyPay provider information [ Possible Values : https://docs.tosspayments.com/reference/enum-codes#%EA%B0%84%ED%8E%B8%EA%B2%B0%EC%A0%9C-%EA%B2%B0%EC%A0%9C-%EC%88%98%EB%8B%A8 ]
+		DiscountAmount float64 `json:"discountAmount"` // Discounted amount
+	} `json:"easyPay"` // EasyPay information, NULLABLE
+	Failure *struct {
+		Code    string `json:"code"`    // Error code
+		Message string `json:"message"` // Reason of error
+	} `json:"failure"` // Error information, NULLABLE
+	Country     string `json:"country"` // ISO 3166-1 alpha-2 country code
 	CashReceipt *struct {
-		Type          string `json:"type"`          // Cash receipt type, [ Possible Values : "소득공제", "지출증빙" ]
-		Amount        int64  `json:"amount"`        // Cash receipt amount
-		TaxFreeAmount int64  `json:"taxFreeAmount"` // Tax-free
-		IssueNumber   string `json:"issueNumber"`   // Cash receipt issue number
-		ReceiptUrl    string `json:"receiptUrl"`    // Cash receipt receipt URL
-	}
+		Type          string  `json:"type"`          // Cash receipt type, [ Possible Values : "소득공제", "지출증빙" ]
+		Amount        float64 `json:"amount"`        // Cash receipt amount
+		TaxFreeAmount int64   `json:"taxFreeAmount"` // Tax-free
+		IssueNumber   string  `json:"issueNumber"`   // Cash receipt issue number
+		ReceiptUrl    string  `json:"receiptUrl"`    // Cash receipt receipt URL
+	} `json:"cashReceipt"` // Cash receipt information, NULLABLE
 	Discount *struct {
-		Amount int64 `json:"amount"` // Discount amount
-	}
+		Amount float64 `json:"amount"` // Discount amount
+	} `json:"discount"` // Discount information, NULLABLE
 }
 
 type RefundReceiveAccount struct {
